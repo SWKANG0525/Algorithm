0a988da0a899e5ac6271d15e87868be77152ea21
SWKANG0525 <stardustsw8947@gmail.com>
2020-07-01 13:16:37 +0900

    BOJ 3053

---
+++ BOJ 3053.py
@@ -0,0 +1,11 @@
+# Author : Kang Ho Dong
+# Date : 2020 - 07 - 01
+# Title : BOJ 3053
+# Language : Python 3
+
+import math
+
+radius = int(input())
+print(round(math.pi*radius*radius,6))
+print(round(2*radius*radius,6))
+
