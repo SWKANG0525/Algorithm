732652a945cf598c468f8ef6cc5538a0e32b3ee5
SWKANG0525 <stardustsw8947@gmail.com>
2020-07-14 00:14:29 +0900

    Fix BOJ 2751 (Heap Sort)

--- BOJ 2751 (Heap Sort).py
+++ BOJ 2751 (Heap Sort).py
@@ -9,28 +9,26 @@ N = int(sys.stdin.readline())
 
 def heapify(idx):
 
-    if idx * 2 + 2 < N:
-        big_elements = 2 * idx + 1 if complete_binary_tree[2 * idx + 1] > complete_binary_tree[2 * idx + 2] else 2 * idx + 2
-        if complete_binary_tree[big_elements] > complete_binary_tree[idx]:
+    big_elements = idx * 2 + 1
+    if big_elements +1 < N and complete_binary_tree[big_elements] < complete_binary_tree[big_elements+1]:
+        big_elements = idx * 2 + 2
+
+    if big_elements < N:
+        if complete_binary_tree[idx] < complete_binary_tree[big_elements]:
             complete_binary_tree[idx], complete_binary_tree[big_elements] = complete_binary_tree[big_elements], complete_binary_tree[idx]
-        heapify(idx*2+1)
-        heapify(idx*2+2)
-    elif idx*2 + 1 < N:
-        big_elements = 2 * idx + 1
-        if complete_binary_tree[big_elements] > complete_binary_tree[idx]:
-            complete_binary_tree[idx], complete_binary_tree[big_elements] = complete_binary_tree[big_elements], complete_binary_tree[idx]
-        heapify(idx*2+1)
-    else:
-        return
+        heapify(big_elements)
+
 
 for i in range (0, N):
     complete_binary_tree.append(int(sys.stdin.readline()))
 
-heapify(0)
+for i in range(N//2 -1 ,-1,-1):
+    heapify(i)
+
 for i in range(N-1,0,-1):
-    heapify(0)
     complete_binary_tree[0],complete_binary_tree[i] = complete_binary_tree[i],complete_binary_tree[0]
-    N=N-1
+    N= N-1
+    heapify(0)
 
 for i in complete_binary_tree:
     print(i)
